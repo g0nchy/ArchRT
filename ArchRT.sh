a8fdb4c582637fb4cd4ca6a49be5700660430288
g0nchy <134018418+g0nchy@users.noreply.github.com>
2023-06-26 00:15:35 -0300

    Update ArchRT.sh
    Script hecho desde cero de nuevo

--- ArchRT.sh
+++ ArchRT.sh
@@ -18,43 +18,31 @@ fi
 # Set the current directory as the dotfiles directory
 ruta=$(dirname "$0")
 
+#!/bin/bash
+
 # Update system
 sudo pacman -Syu --noconfirm
 
-# Install dependencies
-sudo pacman -S --needed --noconfirm git base-devel cmake make yay
+# Installl bspwm, sxhkd y dmenu
+sudo pacman -S --noconfirm bspwm sxhkd dmenu
 
-# Install bspwm, picom, polybar, sxhkd, dmenu, rofi
-yay -S --noconfirm bspwm picom polybar sxhkd dmenu rofi
+# Configurate bspwmrc
+echo "exec bspwm" > ~/.config/bspwm/bspwmrc
 
-# Set wallpaper using feh
-wallpaper_path="$ruta/Wallpapers/simple.png"
-feh --bg-fill "$wallpaper_path"
+# Configurate sxhkdrc
+echo "super + Return" >> ~/.config/sxhkd/sxhkdrc
+echo "    kitty &" >> ~/.config/sxhkd/sxhkdrc
 
-# Copy dotfiles
-mkdir -p ~/.config
-cp -r "$ruta/.config/bspwm" ~/.config/
-cp -r "$ruta/.config/sxhkd" ~/.config/
-cp -r "$ruta/.config/polybar" ~/.config/
-cp -r "$ruta/.config/kitty" ~/.config/
-cp -r "$ruta/.config/rofi" ~/.config/
+# Configurate dmenu
+echo "#!/bin/bash" > ~/.dmenurc
+echo "dmenu_run" >> ~/.dmenurc
 
-# Enable launch on startup
+# Giving execute permission
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/sxhkd/sxhkdrc
-chmod +x ~/.config/polybar/launch.sh
+chmod +x ~/.dmenurc
 
-# Add bspwm, sxhkd, polybar to Xinitrc
-echo "exec bspwm" >> ~/.xinitrc
-echo "sxhkd &" >> ~/.xinitrc
-echo "polybar -r main &" >> ~/.xinitrc
-
-# Configure kitty
-sudo update-alternatives --config x-terminal-emulator
-sed -i 's/urxvt/kitty/g' ~/.config/bspwm/bspwmrc
-
-# Configure bspwmrc
-sed -i 's/rofi -show/rofi -show drun/g' ~/.config/bspwm/bspwmrc
+echo "Instalation and configuration of bspwm, sxhkd and dmenu completed."
 
 # Display completion message
 echo "ArchRT setup complete. Do you want to restart your system now? (y/n)"
